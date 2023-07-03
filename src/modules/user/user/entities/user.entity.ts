4e53b84bc3a258af00187c01e9d94173de56b2dd
Chetos26 <rlz.romero@yavirac.edu.ec>
2023-07-03 18:06:28 -0500

    .

--- src/modules/user/user/entities/user.entity.ts
+++ src/modules/user/user/entities/user.entity.ts
@@ -1,67 +1,74 @@
-import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinTable, ManyToMany, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { Subject } from "src/modules/academic/subjects/entities/subject.entity";
-import { Exclude } from "class-transformer";
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Subject } from 'src/modules/academic/subjects/entities/subject.entity';
+import { Exclude } from 'class-transformer';
 
 @Entity('usuario')
 export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  //////////////////////////////////////////////////////////////
+  @Column('varchar', {
+    name: 'nombre_u',
+    length: 20,
+    nullable: false,
+    comment: 'Nombre del usuario',
+  })
+  nombre_u: string;
 
-    //////////////////////////////////////////////////////////////
-    @Column('varchar',{
-        name: 'nombre_u',
-        length: 20,
-        nullable: false,
-        comment: 'Nombre del usuario',    
-    })
-    nombre_u: string;
+  @Column('varchar', {
+    name: 'apellido_u',
+    length: 20,
+    nullable: false,
+    comment: 'Apellido del usuario',
+  })
+  apellido_u: string;
 
-    @Column('varchar',{
-        name: 'apellido_u',
-        length: 20,
-        nullable: false,
-        comment: 'Apellido del usuario',    
-    })
-    apellido_u: string;
+  @Column('varchar', {
+    name: 'telf',
+    length: 10,
+    nullable: true,
+    comment: 'Teléfono del usuario',
+  })
+  telf: string;
 
-    @Column('varchar',{
-        name: 'telf',
-        length: 9,
-        nullable: true,
-        comment: 'Teléfono del usuario',    
-    })
-    telf: string;
+  @Column('varchar', {
+    name: 'email',
+    unique: true,
+    length: 30,
+    nullable: false,
+    comment: 'Correo del usuario',
+  })
+  email: string;
 
-    @Column('varchar',{
-        name: 'email',
-        unique: true,
-        length: 30,
-        nullable: false,
-        comment: 'Correo del usuario',    
-    })
-    email: string;
+  @Exclude()
+  @Column('varchar', {
+    name: 'contrasenia',
+    length: 255,
+    nullable: false,
+    comment: 'Contraseña del usuario',
+  })
+  contrasenia: string;
 
-   @Exclude()
-    @Column('varchar',{
-        name: 'contrasenia',
-        length: 255,
-        nullable: false,
-        comment: 'Contraseña del usuario',    
-    })
-    contrasenia: string;
-    
-    @Column('varchar',{
-        name: 'nickname',
-        length: 20,
-        unique: true,
-        nullable: false,
-        comment: 'Nickname del usuario',    
-    })
-    nickname: string;
+  @Column('varchar', {
+    name: 'nickname',
+    length: 20,
+    unique: true,
+    nullable: false,
+    comment: 'Nickname del usuario',
+  })
+  nickname: string;
 
-
-    @OneToMany(type=>Subject,subject=>subject.user)
-    subject: Subject[];
-
+  @OneToMany((type) => Subject, (subject) => subject.user)
+  subject: Subject[];
 }
